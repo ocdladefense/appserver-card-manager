3e96ab0afd579fe63b5e7aa0f56b56bb3e246d6f
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-05-03 09:12:53 -0700

    Added a little bootstrap.

--- templates/cards.tpl.php
+++ templates/cards.tpl.php
@@ -1,25 +1,39 @@
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<h3>Your Saved Payment Methods</h3>
+<style>
+    .profile{
+        padding: .7vw;
+    }
+</style>
 
-<?php foreach($paymentProfiles as $card): ?>
+<div class="container text-center vw-100">
+    <h3>Your Saved Payment Methods</h3>
+</div>
 
-    <div>
-        <p><?php print $card->firstName() . " " . $card->lastName() . "'s " . $card->type(); ?></p>
-        <p>Card #: <?php print $card->number(); ?></p>
-        <p>Expires on: <?php print $card->expiresOn(); ?></p>
-        <p><strong>Billing Address</strong></p>
-        <p>
-            <?php print $card->address() . ", " . $card->city() . ", " . $card->state() . " " . $card->zip(); ?>
-        </p>
+<div class="container vw-100">
+    <div class="row">
 
-        <button>Delete</button>
-        <button>&nbsp;&nbsp;Edit&nbsp;&nbsp;</button>
+    <?php foreach($paymentProfiles as $card): ?>
+
+        <div class="col-md-4 bg-light px-6 border border-primary profile">
+
+            <p><?php print $card->firstName() . " " . $card->lastName() . "'s " . $card->type(); ?></p>
+            <p>Card #: <?php print $card->number(); ?></p>
+            <p>Expires on: <?php print $card->expiresOn(); ?></p>
+            <p><strong>Billing Address</strong></p>
+            <p>
+                <?php print $card->address() . ", " . $card->city() . ", " . $card->state() . " " . $card->zip(); ?>
+            </p>
+
+            <button class="btn btn-primary">Delete</button>
+            <button class="btn btn-primary">&nbsp;&nbsp;Edit&nbsp;&nbsp;</button>
+            
+        </div>
+
+
+
+    <?php endforeach; ?>
+
     </div>
-
-
-
-    <br />
-    <br />
-
-<?php endforeach; ?>
+</div>
