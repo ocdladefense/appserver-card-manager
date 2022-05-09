117a7f8ad027070edd1f3eafbab75ae5dbc53c03
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-05-09 12:09:34 -0700

    Remove the link to card-manager.js

--- templates/cards.tpl.php
+++ templates/cards.tpl.php
@@ -2,8 +2,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="<?php print module_path(); ?>/assets/css/card-list.css" />
 
-<script type="application/javascript" src="<?php print module_path(); ?>/assets/js/card-manager.js"></script>
-
 
 
 
