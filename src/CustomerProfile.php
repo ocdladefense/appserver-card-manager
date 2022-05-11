2242a66652d7af96fb4858a4d63c49edfc3b97b1
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-11 13:30:28 -0700

    Let the route for saving customer take a contactId.

--- src/CustomerProfile.php
+++ src/CustomerProfile.php
@@ -1,7 +1,6 @@
 <?php
 
-use PaymentProfile;
-use MerchantAuthentication;
+
 use net\authorize\api\contract\v1 as AnetAPI;
 use net\authorize\api\controller as AnetController;
 use net\authorize\api\constants\ANetEnvironment;
