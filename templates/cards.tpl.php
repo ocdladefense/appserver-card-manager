a43cd043bd835ea7260aa41894545575df9abf93
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-05-18 10:39:56 -0700

    Improve the look of the card list.

--- templates/cards.tpl.php
+++ templates/cards.tpl.php
@@ -15,43 +15,45 @@
     </div>
 <?php endif; ?>
 
+
 <div class="container vw-100">
     <div class="row">
 
-    <?php foreach($paymentProfiles as $card): ?>
+        <?php foreach($paymentProfiles as $card): ?>
 
-        <div class="col-md-4 bg-light px-6 border border-primary profile">
+            <div class="card m-1">
+                <div class="card-body">
+                    
+                    <p class="card-title">
+                        <?php print $card->firstName() . " " . $card->lastName() . "'s " . $card->type(); ?>
+                        <?php if($card->isDefault()) print "(default)"; ?> 
+                    </p>
 
-            <div class="section mb-4">
-                <p>
-                    <?php print $card->firstName() . " " . $card->lastName() . "'s " . $card->type(); ?>
-                    <?php if($card->isDefault()) print "(default)"; ?> 
-                </p>
-                <p>Card ending in &bull;&bull;&bull;&bull; <?php print $card->lastFour(); ?></p>
-                <p>Expires on: <?php print $card->expiresOn(); ?></p>
+                    <p>Card ending in &bull;&bull;&bull;&bull; <?php print $card->lastFour(); ?></p>
+                    <p>Expires on: <?php print $card->expiresOn(); ?></p>
+
+                    <div class="section mb-4 mt-2">
+                        <p><strong>Billing Information</strong></p>
+                        <p><?php print $card->firstName() . " " . $card->lastName(); ?></p>
+                        <p><?php print $card->address(); ?></p>
+                        <p><?php print $card->city() . ", " . $card->state() . " " . $card->zip(); ?></p>
+                        <p><?php print $card->phone(); ?></p>
+                    </div>
+
+                    <form class="btn-form" action="/card/delete/<?php print $card->id(); ?>" onSubmit="return confirm('Are you sure you want to delete this payment method?');">
+                        <button class="btn btn-primary">Delete</button>
+                    </form>
+
+                    <form class="btn-form" action="/card/edit/<?php print $card->id(); ?>" >
+                        <button class="btn btn-primary">Edit Card</button>
+                    </form>
+
+                </div>
             </div>
 
-            <div class="section mb-4">
-                <p><strong>Billing Information</strong></p>
-                <p><?php print $card->firstName() . " " . $card->lastName(); ?></p>
-                <p><?php print $card->address(); ?></p>
-                <p><?php print $card->city() . ", " . $card->state() . " " . $card->zip(); ?></p>
-                <p><?php print $card->phone(); ?></p>
-            </div>
-
-            <form class="btn-form" action="/card/delete/<?php print $card->id(); ?>" onSubmit="return confirm('Are you sure you want to delete this payment method?');">
-                <button class="btn btn-primary">Delete</button>
-            </form>
-
-            <form class="btn-form" action="/card/edit/<?php print $card->id(); ?>" >
-                <button class="btn btn-primary">Edit Card</button>
-            </form>
-            
-        </div>
 
 
-
-    <?php endforeach; ?>
+        <?php endforeach; ?>
 
     </div>
 </div>
