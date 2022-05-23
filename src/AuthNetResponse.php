08c57b18cee7c294763782a2edba75bbc5af67ea
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-22 20:47:34 -0700

    Pass authnet response to this class.

--- src/AuthNetResponse.php
+++ src/AuthNetResponse.php
@@ -1,15 +1,29 @@
 <?php
 
 
-use net\authorize\api\contract\v1 as AnetAPI;
-use net\authorize\api\controller as AnetController;
-use net\authorize\api\constants\ANetEnvironment;
+use net\authorize\api\contract\v1 as AuthNetAPI;
+use net\authorize\api\controller as AuthNetController;
+use net\authorize\api\constants\AuthNetEnvironment;
 
 
 
-class AuthnetResponse {
+class AuthNetResponse {
+
+
+    const RESPONSE_OK = "Ok";
+
+
+    private $resp;
+
     
-    // Theses errors are probably due to programming errors, so Im just gonna throw the exception in the calling code.
+
+
+    public function __construct($resp) {
+        $this->resp = $resp;
+    }
+    
+    // Theses errors are probably due to programming errors,
+    // so I'm just gonna throw the exception in the calling code.
     public function hasErrors($response) {
 
         return $response->getMessages()->getResultCode() != self::RESPONSE_OK;
@@ -23,7 +37,7 @@ class AuthnetResponse {
 
 
     // Some errors should be handled in a user-friendly way...hence the next two methods.
-    //(Feels like im on the verge of refactoring the way I work with the response)
+    // (Feels like I'm on the verge of refactoring the way I work with the response)
     public function getErrorMessage() {
 
         return $this->response->getMessages()->getMessage()[0]->getText();
